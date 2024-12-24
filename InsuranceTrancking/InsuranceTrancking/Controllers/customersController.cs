5fd623b2b781aef203d5e782b485e03ed468bc6d
Talha Kasikci <talhakasikci377334@gmail.com>
2024-12-24 15:48:43 +0300

    attım göt

---
+++ InsuranceTrancking/InsuranceTrancking/Controllers/customersController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InsuranceTrancking.Models;
+
+namespace InsuranceTrancking.Controllers
+{
+    public class customersController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: customers
+        public ActionResult Index()
+        {
+            return View(db.customers.ToList());
+        }
+
+        // GET: customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customers customers = db.customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // GET: customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public ActionResult Create([Bind(Include = "CustomerID,FirstName,LastName,PhoneNumber,Email,Address,IsAdmin")] customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customers.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customers);
+        }
+
+        // GET: customers/Edit/5
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customers customers = db.customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public ActionResult Edit([Bind(Include = "CustomerID,FirstName,LastName,PhoneNumber,Email,Address,IsAdmin")] customers customers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customers);
+        }
+
+        // GET: customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customers customers = db.customers.Find(id);
+            if (customers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customers);
+        }
+
+        // POST: customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+       // [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            customers customer = db.customers.Find(id);
+            vehicles vehicles = db.vehicles.Find(id);
+            insurance_policies insurance_Policies = db.insurance_policies.Find(id);
+            accident_reports accident = db.accident_reports.Find(id);
+            driving_license dl = db.driving_license.Find(id);
+            payments payments = db.payments.Find(id);
+            insurance_companies ic = db.insurance_companies.Find(id);
+            repair_shops repair_Shops = db.repair_shops.Find(id);
+            if (customer != null)
+            {
+                db.customers.Remove(customer);
+                db.vehicles.Remove(vehicles);
+                db.insurance_policies.Remove(insurance_Policies);
+                db.accident_reports.Remove(accident);
+                db.driving_license.Remove(dl);
+                db.payments.Remove(payments);
+                db.insurance_companies.Remove(ic);
+                db.repair_shops.Remove(repair_Shops);
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
